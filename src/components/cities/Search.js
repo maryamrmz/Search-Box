12ac5f5b225a4bbeacc1ee96aa653fb8ac2c762f
Mehrad Rousta <mehrad77@gmail.com>
2019-10-12 17:03:01 +0330

    convert Class Component to Functional one

--- src/components/cities/Search.js
+++ src/components/cities/Search.js
@@ -1,72 +1,52 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { DatePicker } from 'react-persian-datepicker';
+import React, { useState } from "react";
+import { DatePicker } from "react-persian-datepicker";
 
-export class Search extends Component {
-	state = {
-		text: ''
-	};
+const styles = {
+  calendarContainer: "calendarContainer",
+  dayPickerContainer: "dayPickerContainer",
+  monthsList: "monthsList",
+  daysOfWeek: "daysOfWeek",
+  dayWrapper: "dayWrapper",
+  selected: "selected",
+  heading: "heading"
+};
 
-	static propTypes = {
-		searchCities: PropTypes.func.isRequired
-	};
-
-	onSubmit = e => {
-		e.preventDefault();
-		this.props.searchCities(this.state.text);
-		this.setState({ text: '' });
-	};
-
-	onChange = e => this.setState({ text: e.target.value });
-
-	render() {
-		const styles = {
-			calendarContainer: 'calendarContainer',
-			dayPickerContainer: 'dayPickerContainer',
-			monthsList: 'monthsList',
-			daysOfWeek: 'daysOfWeek',
-			dayWrapper: 'dayWrapper',
-			selected: 'selected',
-			heading: 'heading'
-		};
-		return (
-			<div>
-				<form onSubmit={this.onSubmit} className='form'>
-					<div className='mt-5 form-row align-items-center'>
-						<div className='col-6'>
-							<input
-								className='text-center'
-								type='text'
-								name='text'
-								placeholder='شهـــر'
-								value={this.state.text}
-								onChange={this.onChange}
-							/>
-						</div>
-						<div className='col-2'>
-							<DatePicker
-								placeholder='از تاریخ'
-								calendarStyles={styles}
-							/>
-						</div>
-						<div className='col-2'>
-							<DatePicker
-								placeholder='تا تاریخ'
-								calendarStyles={styles}
-							/>
-						</div>
-						<div className='col-1'>
-							<input
-								type='submit'
-								value='جستجــو'
-								className='btn btn-primary'
-							/>
-						</div>
-					</div>
-				</form>
-			</div>
-		);
-	}
-}
+const Search = ({searchCities}) => {
+  const [text, setText] = useState("");
+  const onChange = (e) => {
+    setText(e.target.value)
+    console.log({text, value:e.target.value})
+  }
+  const onClick = e => {
+    searchCities(text);
+  }
+  return (
+    
+        <div className="mt-5 form-row align-items-center">
+          <div className="col-6">
+            <input
+              className="text-center"
+              type="text"
+              name="text"
+              placeholder="شهـــر"
+              value={text}
+              onChange={onChange}
+            />
+          </div>
+          {/* <div className="col-2">
+            <DatePicker placeholder="از تاریخ" calendarStyles={styles} />
+          </div>
+          <div className="col-2">
+            <DatePicker placeholder="تا تاریخ" calendarStyles={styles} />
+          </div> */}
+          <div className="col-1">
+            <button className="btn btn-primary" onClick={onClick}>
+            جستجــو
+            </button>
+          </div>
+        </div>
+      
+  );
+};
 
 export default Search;
